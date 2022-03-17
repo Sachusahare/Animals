79a193cb6ba19bbfdd154f488d06d66e126da929
Sachin Vishwanath Sahare <sachusahare2519@gmail.com>
2022-03-18 03:42:02 +0530

    modified files

---
+++ Tiger.sh
@@ -0,0 +1,46 @@
+echo "My Heart is like the cabbage"
+
+date
+
+echo "Divide it into two, The leaves I give to everyone"
+
+pwd
+
+echo "my heart I will git it to you"
+
+echo "===================================================="
+
+echo "The day I saw You, I crowded my dear"
+
+echo "Not for your beauty or your Tone"
+
+echo "But I simply feel, as-if you are my own"
+
+echo "========================================"
+
+echo "========================================"
+
+echo "Ukhadalalaya Aalu-la Naka Tumhe chilu"
+
+echo "Aamche Rao disto Dukara che Pillu"
+
+echo "=========================================="
+
+echo "kedi che panna adha-madha tunn phatte"
+
+echo "Pooja cha naau ghatla tr jiv ghabarlaya sarkha wate"
+
+
+echo "Without wife very happy life" >> file2
+
+echo "When I have had baby I realise without wife incomplete life" >> file2
+
+cat file2 >> file3
+
+echo "e-dost teri chahat ka ishar Q hai," >> file3
+
+echo "inkar hai magar chahat ka ikrar Q hai." >> file3
+
+echo "Nahi milna hai hamari kismat me shayad," >> file3
+
+echo "fir be E-dost, har ek mod pr tera intazar Q hai" >> file3
